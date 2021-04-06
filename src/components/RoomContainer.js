42393bd864913d39815ef1cbdbd888e3ef595b4e
Sankalp <schaturvedi1008@gmail.com>
2021-04-06 19:25:08 +0530

    sankalp first react

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import RoomFilter from './RoomFilter';
+import {RoomConsumer} from '../Context';
+import Loading from './Loading';
+import RoomList from './RoomList';
+export default function RoomContainer() {
+    return (
+        <RoomConsumer>
+            {value =>{
+                const{loading, sortedRooms, rooms} = value;
+                if (loading){
+                    return <Loading/>
+                }
+            return(
+                
+                <>
+                  
+                    <RoomFilter rooms={rooms} />
+                    <RoomList rooms= {sortedRooms} />
+               </>
+
+            );
+            }}
+
+           
+        </RoomConsumer>
+    );
+}
---
+++ src/pages/Rooms.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Banner from '../components/banner';
+import Hero from '../components/Hero';
+import RoomContainer from '../components/RoomContainer';
+export default function Rooms() {
+    return (
+        <>
+            <Hero hero="roomsHero">
+                <Banner title="our rooms" >
+                    <Link to="/" className="btn-primary">
+                        return home
+                    </Link>
+
+                </Banner>
+            </Hero>
+            <RoomContainer/>
+            </>
+    );
+}
